f6b4d5c688dedbe27a0a7db883e76f8e590b0a63
Sami-Sial <samisial1555@gmail.com>
2026-01-20 21:15:26 +0500

    resolve cors issue

--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -12,6 +12,8 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   })
 );
 app.use(cookieParser());
